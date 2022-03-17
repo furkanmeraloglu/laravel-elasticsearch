fe99e367a5c89097962848c1e68f8ae21782de6b
Furkan Meraloglu <furkanmeraloglu@ip-192-168-1-199.eu-west-2.compute.internal>
2022-03-17 20:16:28 +0300

    Laravel-like package migration published

--- database/migrations/2022_03_17_170112_create_posts_table.php
+++ database/migrations/2022_03_17_170112_create_posts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +17,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
+            $table->longText('content');
+            $table->string('title');
             $table->timestamps();
         });
     }
